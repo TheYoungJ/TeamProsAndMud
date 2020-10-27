3c76d866fc9cddb4e1a2fd4b9edf23037f824690
WalterMitty2112 <devonhope20@gmail.com>
2020-10-26 20:16:02 -0400

    updoot

--- apitest_Utelly/src/main/java/apitest_Utelly.java
+++ apitest_Utelly/src/main/java/apitest_Utelly.java
@@ -1,28 +1,35 @@
-package apitest;
+package apitest_Utelly;
 
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.Response;
 import java.io.IOException;
 
-public class apitest{
-	public static Response get_request() throws IOException {
+public class apitest_Utelly{
+
+	public static Response getRequest(String term, String country){
+		
 		OkHttpClient client = new OkHttpClient();
 
+		String url = "https://utelly-tv-shows-and-movies-availability-v1.p.rapidapi.com/lookup?term="+term+"&country="+country
+
 		Request request = new Request.Builder()
-			.url("https://unogs-unogs-v1.p.rapidapi.com/aaapi.cgi")
+			.url(url)
 			.get()
-			.addHeader("x-rapidapi-host", "unogs-unogs-v1.p.rapidapi.com")
+			.addHeader("x-rapidapi-host", "utelly-tv-shows-and-movies-availability-v1.p.rapidapi.com")
 			.addHeader("x-rapidapi-key", "dd37d15cf1msha20c25bf6ba08c7p1c3821jsn93385a10f1f6")
 			.build();
 
 		Response response = client.newCall(request).execute();
 		return response;
 	}
-
+	
 	public static void main(String[] args){
+		String term = "The Office"
+		String country = "ca"
+		
 		try{
-			Response get_r = get_request();
+			Response get_r = getRequest(term, country);
 			System.out.println(get_r);
 		}
 		catch(IOException e){ System.out.println("ERROR: " + e.toString());}
